e4d2d6cdedd0f27187a6f6c7eed72d8af1e428d9
Alex Purfield <alexpurfield@gmail.com>
2024-01-11 16:54:59 -0700

    started js

---
+++ RockPaperScissors/script.js
@@ -0,0 +1,10 @@
+var wins = 0;
+var losses = 0;
+var ties=0;
+
+var choices = ["R", "P", "S"];
+
+var playGame = function () {
+    var userChoice = window.prompt("Choose R (Rock), P, (Paper), or S (Scissors)");
+    if (!userChoice) {return}
+}
