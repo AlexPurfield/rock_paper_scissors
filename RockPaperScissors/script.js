3ab78e301f0c197e150d800d011afa8ec804944b
Alex Purfield <alexpurfield@gmail.com>
2024-01-11 17:29:14 -0700

    added logic to catch user if they choose outside of r p and s

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -6,10 +6,17 @@ var choices = ["R", "P", "S"];
 
 var playGame = function () {
     var userChoice = window.prompt("Choose R (Rock), P, (Paper), or S (Scissors)");
-    if (!userChoice) {return};
+    
+    if (!userChoice) {return;}
     
     userChoice = userChoice.toUpperCase(); 
     //reassigning var userChoice witht the to upper case method to read the users input no matter what case they use
+
+    if (choices.indexOf(userChoice) === -1) {
+        window.alert("Please choose R, P, or S");
+        playGame();
+        return;
+    }
 
     var index= Math.floor(Math.random() * choices.length);
     var computerChoice = choices[index];
@@ -28,5 +35,12 @@ if (userChoice ===computerChoice) {ties++; window.alert("It's a tie!");
     losses++;
     window.alert("You lose :(");
 }
+window.alert (
+    "Stats: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+);
+var playAgain = window.confirm ("Do you want to play again?");
+if (playAgain) {
+    playGame();
 }
+};
 playGame();
